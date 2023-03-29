99bd52a9fae6d24a042c8525aa21575b9fabc3fb
Saurav <sp.gharti@gmail.com>
2023-03-29 13:28:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-bird-2",
+  url: "https://fragrant-bird-2.botics.co",
+  api: "https://fragrant-bird-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
